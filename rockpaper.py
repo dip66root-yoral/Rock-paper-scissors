bc2c2455eec4b55ce9f21cd2859ef44983559f03
Dipanjan Karmakar <dip06karmakar@gmail.com>
2026-01-01 21:38:36 +0530

    Update rockpaper.py
    we can play rock paper scissors here and it has a voice assisant

--- rockpaper.py
+++ rockpaper.py
@@ -40,40 +40,5 @@ while True:
 
 
 
-# import random
-# import pyttsx3
-
-# emoji ={'r': '💎' ,'p': '📄', 's': '✂️'}
-# choices = ('r', 'p', 's')
-# text_to_speech = pyttsx3.init()
-# user_choice = input("Rock , paper, or scissors? (r/p/s): ").lower()
-# if user_choice not in choices:
-#     print("Invalid choice! Please choose 'r', 'p', or 's'.")
-# while True:
-
-#     computer_choice = random.choice(choices)
-#     print(f'Computer chose {emoji[computer_choice]}')
-#     print(f'User chose {emoji[user_choice]}')
-#     if user_choice== computer_choice:
-#         print("Tie!")
-#         text_to_speech.say("IT'S A TIE")
-#         text_to_speech.runAndWait()
-#     elif  (
-#      (user_choice == 'r' and computer_choice == 's') or
-#      (user_choice == 'p' and computer_choice == 'r') or
-#      (user_choice == 's' and computer_choice == 'p')):
-        
-#         text_to_speech.say("you won")
-#         text_to_speech.runAndWait()
-#         print("you won")
-#     else:
-#         text_to_speech.say("you lost")
-#         text_to_speech.runAndWait()
-#         print("You lost")
-#     should_continue =input("Continue? (y/n): ").lower()
-#     if should_continue == 'n':
-#         print("Thanks for playing!")
-#         break
 
 
-# text_to_speech.stop()
